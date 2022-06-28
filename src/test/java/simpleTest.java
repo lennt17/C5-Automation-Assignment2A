822c6c9a6673a25f682869b63ec3cea07d40ba8b
lennt17 <lennt2k@gmail.com>
2022-06-28 10:34:03 +0700

    add assertTrue

--- src/test/java/simpleTest.java
+++ src/test/java/simpleTest.java
@@ -1,14 +1,10 @@
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import io.restassured.RestAssured;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import listener.TestNGListener;
-import org.testng.ITestNGListener;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -16,10 +12,15 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
+import static org.testng.Assert.assertTrue;
 
 public class simpleTest extends TestNGListener {
     Map<String, Object> mapPost = new HashMap<>();
     Map<String, Object> mapPut = new HashMap<>();
+    private String nameProject = "Shopping List";
+    private String idProjectCreated ;
+
+    String nameProjectUpdate = "Things To Buy";
 
     Gson g = new Gson();
     public simpleTest(){
@@ -31,7 +32,7 @@ public class simpleTest extends TestNGListener {
         RestAssured.baseURI = "https://api.todoist.com/rest/v1";
         basePath = "/projects";
 
-        mapPost.put("name", "Shopping List");
+        mapPost.put("name", nameProject);
 
         System.out.println(accessToken);
         Response res = given()
@@ -40,40 +41,53 @@ public class simpleTest extends TestNGListener {
                 .and()
                 .body(mapPost)
                 .when()
-                .post()
-                .then()
-                .extract().response();
+                .post();
+
         res.prettyPrint();
+        Object re = res.as(Object.class);
+        String a = g.toJson(re);
+        JsonObject j = g.fromJson(a, JsonObject.class);
+        idProjectCreated = j.get("id").getAsString();
+        assertTrue((j.get("name").getAsString()).equals(nameProject));
     }
 
     @Test(description = "Get a project")
     public void Test02_get_A_Project() {
-        final String GET_PROJECT = "/2293619235";
-        Response response = given()
+        final String GET_PROJECT = "/" + idProjectCreated;
+        Response respon = given()
                 .contentType(ContentType.JSON)
                 .headers("authorization", "Bearer " + accessToken)
-                .get(GET_PROJECT)
-                .then()
-                .extract().response();
+                .get(GET_PROJECT);
 
-        Object res = response.as(Object.class);
+        Object res = respon.as(Object.class);
         String a = g.toJson(res);
         JsonObject j = g.fromJson(a, JsonObject.class);
-        System.out.println(j);
+        assertTrue((j.get("id").getAsString()).equals(idProjectCreated));
     }
 
     @Test(description = "Update project")
     public void Test03_updateProject() {
-        mapPut.put("name", "Things To Buy");
+        mapPut.put("name", nameProjectUpdate);
 
-        final String GET_PROJECT = "/2293619235";
-        Response response = given()
+        final String GET_PROJECT = "/" + idProjectCreated;
+        Response resp = given()
                 .header("authorization", "Bearer " + accessToken)
                 .contentType(ContentType.JSON)
                 .and()
                 .body(mapPut)
                 .when()
                 .post(GET_PROJECT);
+
+        // Get name project to verify updated name
+        Response respon = given()
+                .contentType(ContentType.JSON)
+                .headers("authorization", "Bearer " + accessToken)
+                .get(GET_PROJECT);
+
+        Object res = respon.as(Object.class);
+        String a = g.toJson(res);
+        JsonObject j = g.fromJson(a, JsonObject.class);
+        assertTrue((j.get("name").getAsString()).equals(nameProjectUpdate));
     }
 
     @Test(description = "Get all project")
